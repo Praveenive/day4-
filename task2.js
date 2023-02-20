88d36fb93d418cd29b90ea82b30ece5514080d08
Praveenive <124970010+Praveenive@users.noreply.github.com>
2023-02-20 22:29:42 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,15 @@
+var request=new XMLHttpRequest();
+//Step  02:specifiy the API url
+request.open("GET","https://restcountries.com/v2/all");
+//Step 03:sending the request
+request.send();
+//Step 04: Once the requested data is there in server
+//once the data is successfully loaded, server response 
+//will be of 200 status code
+//Functions: they are used to perform specific task
+//Functions applications: used in all js events
+request.onload=function(){
+var result=JSON.parse(request.response);
+for(let i=0;i<result.length;i++)
+console.log(result[i].name,result[i].region,result[i].subregion,result[i].population);
+}
